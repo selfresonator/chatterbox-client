79d287a0e5ce69e85ce922e3238634a8be39820b
selfresonator <selfresonator@gmail.com>
2016-03-25 15:19:20 -0700

    Redo fetch test and correctly passes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,9 @@
 // YOUR CODE HERE:
 var app = {
   init: function() {},
-  send: function() {
-    console.log('hey!');
+
+  send: function(message) {
+
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/chatterbox',
@@ -18,11 +19,36 @@ var app = {
       }
     });
   },
-  fetch: function() {}
+
+  fetch: function() {
+    console.log('hi');
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  clearMessages: function() {  
+    $('#chats').remove();
+  },
+  
+  addMessage: function(message) {
+    console.log('addMessage executed');
+    
+    $('#chats').append(message);
+  },
+
+  server: 'https://api.parse.com/1/classes/chatterbox'
 }; 
 
-var message = {
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  roomname: 'lobby'
-};
+
+
