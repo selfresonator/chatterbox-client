a7fb5875da9d1fa80d1510d6ca31860f6f2d49cc
selfresonator <selfresonator@gmail.com>
2016-03-25 13:53:29 -0700

    Test for sending complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,28 @@
 // YOUR CODE HERE:
 var app = {
   init: function() {},
-  send: function() {},
+  send: function() {
+    console.log('hey!');
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
   fetch: function() {}
 }; 
+
+var message = {
+  username: 'Mel Brooks',
+  text: 'It\'s good to be the king',
+  roomname: 'lobby'
+};
